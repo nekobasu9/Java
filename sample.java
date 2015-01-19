e91e044fab045dadbc046e230ec8e7ce2606f049
nekobasu9 <nekobasu9@gmail.com>
2015-01-19 21:44:37 +0900

    java

---
+++ sample.java
@@ -0,0 +1,11 @@
+public class sample {
+  public static void main (String[] args) {
+  	
+  	/*int x ,y,z;
+  	x=13;
+  	y=17;
+  	z=x+y;*/
+  	System.out.println(13*17);
+  	//System.out.println("\"x="+x+","+"y="+y+"\"");
+  }
+} 
